db01a7abf6cc8e0ff25624f296ba81914261a7dc
GregDigital <68654973+GregDigital@users.noreply.github.com>
2023-02-06 10:03:50 +0100

    contact_page

--- data.js
+++ data.js
@@ -20,7 +20,7 @@ function show(response) {
 
       let html = acc.reduce((a, l) => a + l);
       container.innerHTML = html;
-      light();
+
       displayClose();
     });
   });
@@ -36,43 +36,80 @@ function displayClose() {
   });
 }
 
-function light() {
-  const conta = document.querySelector(".cont");
-  document.querySelector(".slider").addEventListener("input", (e) => {
-    conta.style.setProperty("--position", `${e.target.value}%`);
-  });
-}
-
 function afficher(user) {
   return `
         
         <div class="image-container">
-       
+        <div class="presentation">
+        <div class="titre">
+        <h1>${user.h1}</h1>
+        <h2>identité visuelle | charte graphique | photo | site internet | seo </h2>
+        </div>
         <img
-          class="image-before slider-image"
-         src="../assets/img/${user.before}"
-          alt="color photo"
-        />
-        <img
-          class="image-after slider-image"
-          src="../assets/img/${user.after}"
-          alt="black and white"
-        />
-      </div>
-      <!-- step="10" -->
-      <input
-        type="range"
-        min="0"
-        max="100"
-        value="50"
-        aria-label="Percentage of before photo shown"
-        class="slider"
+        class="card"
+       src="../assets/img/${user.card}"
+        alt="card"
       />
-      <div class="slider-line" aria-hidden="true"></div>
-      <div class="slider-button" aria-hidden="true">
-      
-      
       </div>
+      <p class="pBorder">Lorem ipsum dolor sit amet consectetur. Integer malesuada enim at quam lacinia dignissim viverra. Urna ipsum ut lectus mi eleifend turpis accumsan ipsum at. Porttitor sit ac platea orci enim in. Eu faucibus massa erat mattis vel netus sollicitudin praesent integer. Rhoncus lacus lorem eget in felis lorem aliquet dui. Egestas eget lectus nisi magna elit id elit felis.
+      </p>
+
+      <h3>identité visuelle</h3>
+
+      <div class="presentation_id">
+      <img
+        class="card"
+       src="../assets/img/${user.card_logo}"
+        alt="card"
+      />
+      <div class="identite">
+
+      <h2>logo</h2>
+      <p>Lorem ipsum dolor sit amet consectetur. Integer malesuada enim at quam lacinia dignissim viverra. Urna ipsum ut lectus mi eleifend turpis accumsan ipsum at. Porttitor sit ac platea orci enim in. Eu faucibus massa erat mattis vel netus sollicitudin praesent integer. Rhoncus lacus lorem eget in felis lorem aliquet dui. Egestas eget lectus nisi magna elit id elit felis.
+      </p>
+      </div>
+      </div>
+
+ <div class="card_visit">
+    
+    <div class="card_visit_id">
+
+            <div class="card_visit_p">
+            <h2>carte de visite</h2>
+            <p>
+            aliquet dui. Egestas eget lectus nisi magna elit id elit felis.
+            sollicitudin praesent integer. Rhoncus lacus lorem eget in felis lorem aliquet dui. Egestas eget lectus ni elit id elit felis.
+            </p>
+            </div>
+            <img
+            class="card"
+          src="../assets/img/${user.card_visit_recto}"
+            alt="card"
+          />
+          <img
+          class="card"
+        src="../assets/img/${user.card_visit_verso}"
+          alt="card"
+        />
+       </div>
+       </div>
+       <div class="site">
+       <h3>site internet & seo</h3>
+       <img
+     src="../assets/img/${user.imac}"
+       alt="card"
+     />
+     <p>
+     Lorem ipsum dolor sit amet consectetur. Integer malesuada enim at quam lacinia dignissim viverra. Urna ipsum ut lectus mi eleifend turpis accumsan ipsum at. Porttitor sit ac platea orci enim in. Eu faucibus massa erat mattis vel netus sollicitudin praesent integer. Rhoncus lacus lorem eget in felis lorem aliquet dui. Egestas eget lectus nisi magna elit id elit felis.
+
+     </p>
+      </div>
+    </div>
+   
+     
+      
+      
+ </div>
           `;
 }
 
